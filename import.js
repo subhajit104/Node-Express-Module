8a81bfb5fa228b9825ea44d45bbdc89ac27a4266
Subhajit Manna <subhajit.manna.104@gmail.com>
2018-03-20 23:09:45 +0530

    USing Html Page

--- import.js
+++ import.js
@@ -1,23 +1,51 @@
 //In this section we are going  to make a flip coin case.
 console.log('import.js is started');
 const r = require;
-// const sayHi = r('./export');
-// console.log(sayHi); 
-var coinState = true;
-console.log('-------------------------------------------------');
-console.log('Creating a FlipCoin App');
+const fs = r('fs');
 const express = r('express');
 const app = express();
 
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
+
+var coinState = true;
+console.log('-------------------------------------------------');
+console.log('Creating a FlipCoin App');
+
+app.use((req,res,next) => {
+	
+	console.log("ohh");
+	var string = `this is ${req.method} ${req.url} \n`;
+	fs.appendFile('server.url',string,(error)=>{
+		if(error){
+			console.log('unable to connect the server.log');
+		}
+	})
+	next();
+});
+
 app.get('/',(req,res) => {
-	res.send('Creating A FlipCoin App');
+	res.render('home.html');
 });
 
 //get the coin.
-app.get('/getCoin',(req,res) => {
-  res.send((coinState == true) ? 'Heads' : 'Tails'); 
+app.get('/getCoinState',(req,res) => {
+  var coin = coinState == true ? 'Heads' : 'tail' ;
+  res.render('getCoinState.html', {
+  	coinState:coin
+  });
 });
+
 app.get('/flipCoin',(req,res) => {
+   coinState = !!Math.round(Math.random());
+   res.render('flipCoin.html');
+});
+
+app.listen(3000,() => {
+    console.log('Exaple App listening a port 3000!');
+});
+
+
 	// var  position = Math.random();
 	// if(position < 0.5){
 	// 	res.send('head');
@@ -25,13 +53,9 @@ app.get('/flipCoin',(req,res) => {
 	// 	res.send('tail');
 	// }
     // this is single line code to take a boolean value randomly.
-	coinState = !!Math.round(Math.random());
-	res.send('Done Fliped');
+	// coinState = !!Math.round(Math.random());
+	// res.send('Done Fliped');
 	//another way to do that 
 	// coinState = (Math.random() < 0.5) ? false : true ;
     // anoher a way to that
     //   coinState = Math.round(Math.random()) && true ; 
-})
-app.listen(3000,() => {
-    console.log('Exaple App listening a port 3000!');
-});
